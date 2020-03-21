12607efad0a29c60ecd354c8c3f0bd1af21902c5
Sajal Shrestha <sajal.shres@gmail.com>
2020-03-21 11:09:56 +0545

    Removed unused import

--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -2,11 +2,6 @@ import {
   FETCH_GAME_PENDING,
   FETCH_GAME_SUCCESS,
   FETCH_GAME_ERROR,
-  SUBMIT_GAME_PENDING,
-  SUBMIT_GAME_SUCCESS,
-  SUBMIT_GAME_ERROR,
-  UPDATE_SCORE,
-  RESET_SCORE
 } from './actionTypes';
 
 export const fetchGamePending = () => ({ type: FETCH_GAME_PENDING });
