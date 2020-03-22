156954681414f0c7b80d291f5d334c3a3f496db3
Sajal Shrestha <sajal.shres@gmail.com>
2020-03-22 14:10:53 +0000

    Updated rank table design

--- client/src/components/Rank/index.js
+++ client/src/components/Rank/index.js
@@ -1,9 +1,46 @@
 import React from 'react';
+import './styles.scss';
 
 function Rank() {
   return (
-    <div>
-      <h1>Rank Page</h1>
+    <div className="rank">
+      <h1 className="rank__title">Rank Page</h1>
+      <table className="rank-table">
+        <thead>
+          <tr>
+            <th className="rank-table__name">Name</th>
+            <th className="rank-table__score">Score</th>
+            <th className="rank-table__words">Words</th>
+            <th className="rank-table__date">Date</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Sam</td>
+            <td>40</td>
+            <td>Sam Hello Pen Sit Hit Lit Nit Plunk Plunk Plunk Plunk</td>
+            <td>19-20-2020</td>
+          </tr>
+          <tr>
+            <td>Sam</td>
+            <td>40</td>
+            <td>Sam Hello Pen Sit Hit Lit Nit Plunk</td>
+            <td>19-20-2020</td>
+          </tr>
+          <tr>
+            <td>Sam</td>
+            <td>40</td>
+            <td>Sam Hello Pen Sit Hit Lit Nit Plunk</td>
+            <td>19-20-2020</td>
+          </tr>
+          <tr>
+            <td>Sam</td>
+            <td>40</td>
+            <td>Sam Hello Pen Sit Hit Lit Nit Plunk</td>
+            <td>19-20-2020</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
